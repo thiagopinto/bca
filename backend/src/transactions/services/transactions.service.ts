0bd0845ad1fc54085e2bb43e33a8b56cb716a265
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2025-05-03 10:53:40 -0300

    Delete all transactions

--- backend/src/transactions/services/transactions.service.ts
+++ backend/src/transactions/services/transactions.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { ITransactionRepository } from '../interfaces/transaction-repository.interface'; // Importe a interface
 import { CreateTransactionDto } from '../dto/create-transaction.dto';
 import { Transaction } from '../entities/transaction.entity';
+import { StatisticsResponseDto } from '../../shared/dtos/statistics-response.dto';
 
 @Injectable()
 export class TransactionsService {
@@ -26,5 +27,40 @@ export class TransactionsService {
       transactionTimestamp,
     );
     this.transactionRepository.save(transaction);
+  }
+
+  deleteAllTransactions(): void {
+    this.transactionRepository.deleteAll();
+  }
+
+  getTransactionsInLast60Seconds(): Transaction[] {
+    return this.transactionRepository.getTransactionsInLast60Seconds();
+  }
+
+  getStatistics(): StatisticsResponseDto {
+    const transactions = this.getTransactionsInLast60Seconds();
+
+    if (transactions.length === 0) {
+      return {
+        count: 0,
+        sum: 0,
+        avg: 0,
+        min: 0,
+        max: 0,
+      };
+    }
+
+    const sum = transactions.reduce((acc, curr) => acc + curr.amount, 0);
+    const min = Math.min(...transactions.map((t) => t.amount));
+    const max = Math.max(...transactions.map((t) => t.amount));
+    const avg = sum / transactions.length;
+
+    return {
+      count: transactions.length,
+      sum,
+      avg,
+      min,
+      max,
+    };
   }
 }
